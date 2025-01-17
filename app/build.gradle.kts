d9efd26c2fa60c157b9c72630330cfac5b68275b
lucalux2003 <lucgomez777@gmail.com>
2025-01-17 16:58:44 +0100

    mise à jour version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.projet.projetandroidpokemon"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
